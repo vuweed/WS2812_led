ba3921d12060a7ceca197471febf6bf15979d8d0
vuweed <halucminhvu2000@gmail.com>
2024-06-23 12:05:13 +0700

    ok

--- src/W2812/Untitled-1.c
+++ src/W2812/Untitled-1.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,6 +42,7 @@
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
 DMA_HandleTypeDef hdma_tim1_ch1;
+DMA_HandleTypeDef hdma_tim1_ch2;
 
 /* USER CODE BEGIN PV */
 
@@ -59,6 +60,343 @@ static void MX_TIM1_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+#define MAX_LED 100
+#define USE_BRIGHTNESS 1
+
+
+uint8_t LED_Data[MAX_LED][4];
+uint8_t LED_Mod[MAX_LED][4];  // for brightness
+
+int datasentflag=0;
+
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+{
+    HAL_TIM_PWM_Stop_DMA(&htim1, TIM_CHANNEL_1);
+    datasentflag=1;
+}
+
+void Set_LED (int LEDnum, int Red, int Green, int Blue)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = Green;
+    LED_Data[LEDnum][2] = Red;
+    LED_Data[LEDnum][3] = Blue;
+}
+static void Set_LED_Green(int LEDnum)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = 255;
+    LED_Data[LEDnum][2] = 0;
+    LED_Data[LEDnum][3] = 0;
+}
+static void Set_LED_Blue(int LEDnum)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = 0;
+    LED_Data[LEDnum][2] = 0;
+    LED_Data[LEDnum][3] = 255;
+}
+static void Set_LED_Red(int LEDnum)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = 0;
+    LED_Data[LEDnum][2] = 255;
+    LED_Data[LEDnum][3] = 0;
+}
+static void Set_LED_X1(int LEDnum)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = 255;
+    LED_Data[LEDnum][2] = 255;
+    LED_Data[LEDnum][3] = 0;
+}
+static void Set_LED_X2(int LEDnum)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = 0;
+    LED_Data[LEDnum][2] = 255;
+    LED_Data[LEDnum][3] = 255;
+}
+void led_effect_1()
+{
+    int i = 0;
+    for(i = 0; i < MAX_LED; i++)
+    {
+        Set_LED_Green(i);
+        Set_Brightness(10);
+        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_Blue(i+1);
+        Set_Brightness(20);
+        WS2812_Send();
+        HAL_Delay(10);
+
+        Set_LED_Red(i+2);
+        Set_Brightness(30);
+        WS2812_Send();
+        HAL_Delay(10);
+
+        Set_LED_X1(i+3);
+        Set_Brightness(40);
+        WS2812_Send();
+        HAL_Delay(10);
+
+        Set_LED_X2(i+4);
+        Set_Brightness(45);
+        WS2812_Send();
+        HAL_Delay(10);
+//        HAL_Delay(900);
+    }
+}
+void led_effect_2()
+{
+    int i = 0;
+    for(i = 0; i < MAX_LED; i++)
+    {
+        Set_LED_Green(i);
+
+//        HAL_Delay(10);
+
+        Set_LED_Blue(i+1);
+//        Set_Brightness(20);
+//        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_Red(i+2);
+//        Set_Brightness(30);
+//        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_X1(i+3);
+//        Set_Brightness(40);
+//        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_X2(i+4);
+//        Set_Brightness(45);
+//        WS2812_Send();
+//        HAL_Delay(10);
+//        HAL_Delay(900);
+        Set_Brightness(45);
+        WS2812_Send();
+        HAL_Delay(100);
+    }
+    Reset_LED();
+    HAL_Delay(100);
+
+}
+
+void led_effect_3()
+{
+    int i = 0;
+    for(i = 0; i < MAX_LED; i++)
+    {
+        Set_LED_Red(i);
+
+//        HAL_Delay(10);
+
+        Set_LED_Blue(i+1);
+//        Set_Brightness(20);
+//        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_Green(i+2);
+//        Set_Brightness(30);
+//        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_X1(i+3);
+//        Set_Brightness(40);
+//        WS2812_Send();
+//        HAL_Delay(10);
+
+        Set_LED_X2(i+4);
+//        Set_Brightness(45);
+//        WS2812_Send();
+//        HAL_Delay(10);
+//        HAL_Delay(900);
+        Set_Brightness(45);
+        WS2812_Send();
+        HAL_Delay(100);
+    }
+    Reset_LED();
+    HAL_Delay(100);
+
+}
+#define PI 3.14159265
+
+void Set_Brightness (int brightness)  // 0-45
+{
+#if USE_BRIGHTNESS
+
+    if (brightness > 45) brightness = 45;
+    for (int i=0; i<MAX_LED; i++)
+    {
+        LED_Mod[i][0] = LED_Data[i][0];
+        for (int j=1; j<4; j++)
+        {
+            float angle = 90-brightness;  // in degrees
+            angle = angle*PI / 180;  // in rad
+            LED_Mod[i][j] = (LED_Data[i][j])/(tan(angle));
+        }
+    }
+
+#endif
+
+}
+
+uint16_t pwmData[(24*MAX_LED)+50];
+
+void WS2812_Send (void)
+{
+    uint32_t indx=0;
+    uint32_t color;
+
+
+    for (int i= 0; i<MAX_LED; i++)
+    {
+#if USE_BRIGHTNESS
+        color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
+#else
+        color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
+#endif
+
+        for (int i=23; i>=0; i--)
+        {
+            if (color&(1<<i))
+            {
+                pwmData[indx] = 60;  // 2/3 of 90
+            }
+
+            else pwmData[indx] = 30;  // 1/3 of 90
+
+            indx++;
+        }
+
+    }
+
+    for (int i=0; i<50; i++)
+    {
+        pwmData[indx] = 0;
+        indx++;
+    }
+
+    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx);
+    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_2, (uint32_t *)pwmData, indx);
+    while (!datasentflag){};
+    datasentflag = 0;
+}
+
+void Reset_LED (void)
+{
+    for (int i=0; i<MAX_LED; i++)
+    {
+        LED_Data[i][0] = i;
+        LED_Data[i][1] = 0;
+        LED_Data[i][2] = 0;
+        LED_Data[i][3] = 0;
+    }
+}
+
+
+// ported from the arduino code for 8 LEDs located at ->>>>  https://adrianotiger.github.io/Neopixel-Effect-Generator/
+
+uint16_t effStep = 0;
+
+uint8_t rainbow_effect_left() {
+    // Strip ID: 0 - Effect: Rainbow - LEDS: 8
+    // Steps: 13 - Delay: 54
+    // Colors: 3 (255.0.0, 0.255.0, 0.0.255)
+    // Options: rainbowlen=8, toLeft=true,
+//  if(millis() - strip_0.effStart < 54 * (strip_0.effStep)) return 0x00;
+
+  float factor1, factor2;
+  uint16_t ind;
+  for(uint16_t j=0;j<8;j++) {
+    ind = effStep + j * 1.625;
+    switch((int)((ind % 13) / 4.333333333333333)) {
+      case 0: factor1 = 1.0 - ((float)(ind % 13 - 0 * 4.333333333333333) / 4.333333333333333);
+              factor2 = (float)((int)(ind - 0) % 13) / 4.333333333333333;
+              /************ chnaged here *********/
+
+              Set_LED(j, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2);
+
+              WS2812_Send();
+              break;
+      case 1: factor1 = 1.0 - ((float)(ind % 13 - 1 * 4.333333333333333) / 4.333333333333333);
+              factor2 = (float)((int)(ind - 4.333333333333333) % 13) / 4.333333333333333;
+              Set_LED(j, 0 * factor1 + 0 * factor2, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2);
+              WS2812_Send();
+              break;
+      case 2: factor1 = 1.0 - ((float)(ind % 13 - 2 * 4.333333333333333) / 4.333333333333333);
+              factor2 = (float)((int)(ind - 8.666666666666666) % 13) / 4.333333333333333;
+              Set_LED(j, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2, 255 * factor1 + 0 * factor2);
+              WS2812_Send();
+              break;
+    }
+  }
+  if(effStep >= 13) {effStep=0; return 0x03; }
+  else effStep++;
+  return 0x01;
+}
+
+
+
+uint8_t rainbow_effect_right() {
+    // Strip ID: 0 - Effect: Rainbow - LEDS: 8
+    // Steps: 14 - Delay: 30
+    // Colors: 3 (255.0.0, 0.255.0, 0.0.255)
+    // Options: rainbowlen=8, toLeft=false,
+//  if(millis() - strip_0.effStart < 30 * (strip_0.effStep)) return 0x00;
+  float factor1, factor2;
+  uint16_t ind;
+  for(uint16_t j=0;j<8;j++) {
+    ind = 14 - (int16_t)(effStep - j * 1.75) % 14;
+    switch((int)((ind % 14) / 4.666666666666667)) {
+      case 0: factor1 = 1.0 - ((float)(ind % 14 - 0 * 4.666666666666667) / 4.666666666666667);
+              factor2 = (float)((int)(ind - 0) % 14) / 4.666666666666667;
+              Set_LED(j, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2);
+              WS2812_Send();
+              break;
+      case 1: factor1 = 1.0 - ((float)(ind % 14 - 1 * 4.666666666666667) / 4.666666666666667);
+              factor2 = (float)((int)(ind - 4.666666666666667) % 14) / 4.666666666666667;
+              Set_LED(j, 0 * factor1 + 0 * factor2, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2);
+              WS2812_Send();
+              break;
+      case 2: factor1 = 1.0 - ((float)(ind % 14 - 2 * 4.666666666666667) / 4.666666666666667);
+              factor2 = (float)((int)(ind - 9.333333333333334) % 14) / 4.666666666666667;
+              Set_LED(j, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2, 255 * factor1 + 0 * factor2);
+              WS2812_Send();
+              break;
+    }
+  }
+  if(effStep >= 14) {effStep = 0; return 0x03; }
+  else effStep++;
+  return 0x01;
+}
+void led_move()
+{
+    int i = 0;
+    int R = 0, G = 0, B = 0;
+    for(i = 0; i < MAX_LED; i++)
+    {
+        Set_LED(i, R, G, B);
+        Set_Brightness(10);
+        WS2812_Send();
+        HAL_Delay(30);
+        if(R > 255)
+            R = 0;
+        if(G > 255)
+            G = 0;
+        if(B > 255)
+            B = 0;
+
+        R+=10;
+        G+=10;
+        B+=10;
+    }
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,7 +430,21 @@ int main(void)
   MX_DMA_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
+  Set_LED(0, 255, 0, 0);
+  Set_LED(1, 255, 0, 0);
+  Set_LED(2, 0, 0, 255);
 
+  Set_LED(3, 46, 89, 128);
+
+  Set_LED(4, 156, 233, 100);
+  Set_LED(5, 102, 0, 235);
+  Set_LED(6, 47, 38, 77);
+
+  Set_LED(7, 255, 200, 0);
+
+  Set_Brightness(10);
+  WS2812_Send();
+//  Reset_LED();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -102,7 +454,28 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+//      rainbow_effect_right();
+//      rainbow_effect_left();
+//        HAL_Delay (30);
+      led_move();
+      led_effect_2();
+      led_effect_3();
+//        for (int i=0; i<46; i++)
+//        {
+//            Set_Brightness(i);
+//            WS2812_Send();
+//            HAL_Delay (5);
+//        }
+////
+//        for (int i=45; i>=0; i--)
+//        {
+//            Set_Brightness(i);
+//            WS2812_Send();
+//            HAL_Delay (5);
+//        }
   }
+
   /* USER CODE END 3 */
 }
 
@@ -120,7 +493,7 @@ void SystemClock_Config(void)
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
@@ -192,7 +565,7 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
   sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
   sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
   sBreakDeadTimeConfig.DeadTime = 0;
