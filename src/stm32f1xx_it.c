c46c194c6d819068d5419908e2c992178aa2dca9
vuweed <halucminhvu2000@gmail.com>
2024-07-06 15:59:27 +0700

    tét moving IRQ

--- src/stm32f1xx_it.c
+++ src/stm32f1xx_it.c
@@ -152,106 +152,106 @@ void PendSV_Handler(void)
   * @param  None
   * @retval None
   */
-void TIM2_IRQHandler(void)
-{
-  if (TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET)
-  {
-//    if((STATE_1 != state_flag))
-//    {
-      if(false == toggle_all_led_flag)
-      {
-          if(STATE_1 == state_flag)
-          {
-              sound_value = analogRead(VR_PIN);
-          }
+// void TIM2_IRQHandler(void)
+// {
+//   if (TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET)
+//   {
+// //    if((STATE_1 != state_flag))
+// //    {
+//       if(false == toggle_all_led_flag)
+//       {
+//           if(STATE_1 == state_flag)
+//           {
+//               sound_value = analogRead(VR_PIN);
+//           }
 
-      }
-      else
-      {
-          sound_value = analogRead(VR_PIN);
-      }
-      if(sound_value < 700)
-      {
-        //turn on led
-        state_flag = PROCESSING_1;
-        toggle_all_led_flag = false;
+//       }
+//       else
+//       {
+//           sound_value = analogRead(VR_PIN);
+//       }
+//       if(sound_value < 700)
+//       {
+//         //turn on led
+//         state_flag = PROCESSING_1;
+//         toggle_all_led_flag = false;
 
-      }
-      else
-      {
-//        state_flag = PROCESSING_1;
-        toggle_all_led_flag = true;
-      }
+//       }
+//       else
+//       {
+// //        state_flag = PROCESSING_1;
+//         toggle_all_led_flag = true;
+//       }
 
-//    }
+// //    }
 
 
-    // if(state_flag == STATE_1)
-    // {
-    //     sound_value = analogRead(VR_PIN);
-    //     if(sound_value < 700)
-    //     {
-    //         state_flag = PROCESSING_1;
-    //         toggle_all_led_flag = false;
+//     // if(state_flag == STATE_1)
+//     // {
+//     //     sound_value = analogRead(VR_PIN);
+//     //     if(sound_value < 700)
+//     //     {
+//     //         state_flag = PROCESSING_1;
+//     //         toggle_all_led_flag = false;
 
-    //     }
-    //     else
-    //     {
-    //       state_flag = PROCESSING_2;
-    //       toggle_all_led_flag = true;
-    //     }
-    // }
-    // else if(state_flag == STATE_2)
-    // {
-    //     sound_value = analogRead(VR_PIN);
-    //     if(sound_value < 700)
-    //     {
-    //         state_flag = PROCESSING_1;
-    //         toggle_all_led_flag = false;
+//     //     }
+//     //     else
+//     //     {
+//     //       state_flag = PROCESSING_2;
+//     //       toggle_all_led_flag = true;
+//     //     }
+//     // }
+//     // else if(state_flag == STATE_2)
+//     // {
+//     //     sound_value = analogRead(VR_PIN);
+//     //     if(sound_value < 700)
+//     //     {
+//     //         state_flag = PROCESSING_1;
+//     //         toggle_all_led_flag = false;
 
-    //     }
-    //     else
-    //     {
-    //       state_flag = PROCESSING_2;
-    //       toggle_all_led_flag = true;
-    //     }
-    // }
+//     //     }
+//     //     else
+//     //     {
+//     //       state_flag = PROCESSING_2;
+//     //       toggle_all_led_flag = true;
+//     //     }
+//     // }
 
-     TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
+//      TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
 
-    // /* Pin PC.06 toggling with frequency = 73.24 Hz */
-//     GPIO_WriteBit(GPIOC, GPIO_Pin_13, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_13)));
-     capture = TIM_GetCapture1(TIM2);
-     TIM_SetCompare1(TIM2, capture + CCR1_Val);
-  }
-  // else if (TIM_GetITStatus(TIM2, TIM_IT_CC2) != RESET)
-  // {
-  //   // TIM_ClearITPendingBit(TIM2, TIM_IT_CC2);
+//     // /* Pin PC.06 toggling with frequency = 73.24 Hz */
+// //     GPIO_WriteBit(GPIOC, GPIO_Pin_13, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_13)));
+//      capture = TIM_GetCapture1(TIM2);
+//      TIM_SetCompare1(TIM2, capture + CCR1_Val);
+//   }
+//   // else if (TIM_GetITStatus(TIM2, TIM_IT_CC2) != RESET)
+//   // {
+//   //   // TIM_ClearITPendingBit(TIM2, TIM_IT_CC2);
 
-  //   // /* Pin PC.07 toggling with frequency = 109.8 Hz */
-  //   // GPIO_WriteBit(GPIOC, GPIO_Pin_7, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_7)));
-  //   // capture = TIM_GetCapture2(TIM2);
-  //   // TIM_SetCompare2(TIM2, capture + CCR2_Val);
-  // }
-  // else if (TIM_GetITStatus(TIM2, TIM_IT_CC3) != RESET)
-  // {
-  //   // TIM_ClearITPendingBit(TIM2, TIM_IT_CC3);
+//   //   // /* Pin PC.07 toggling with frequency = 109.8 Hz */
+//   //   // GPIO_WriteBit(GPIOC, GPIO_Pin_7, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_7)));
+//   //   // capture = TIM_GetCapture2(TIM2);
+//   //   // TIM_SetCompare2(TIM2, capture + CCR2_Val);
+//   // }
+//   // else if (TIM_GetITStatus(TIM2, TIM_IT_CC3) != RESET)
+//   // {
+//   //   // TIM_ClearITPendingBit(TIM2, TIM_IT_CC3);
 
-  //   // /* Pin PC.08 toggling with frequency = 219.7 Hz */
-  //   // GPIO_WriteBit(GPIOC, GPIO_Pin_8, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_8)));
-  //   // capture = TIM_GetCapture3(TIM2);
-  //   // TIM_SetCompare3(TIM2, capture + CCR3_Val);
-  // }
-  // else
-  // {
-  //   // TIM_ClearITPendingBit(TIM2, TIM_IT_CC4);
+//   //   // /* Pin PC.08 toggling with frequency = 219.7 Hz */
+//   //   // GPIO_WriteBit(GPIOC, GPIO_Pin_8, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_8)));
+//   //   // capture = TIM_GetCapture3(TIM2);
+//   //   // TIM_SetCompare3(TIM2, capture + CCR3_Val);
+//   // }
+//   // else
+//   // {
+//   //   // TIM_ClearITPendingBit(TIM2, TIM_IT_CC4);
 
-  //   // /* Pin PC.09 toggling with frequency = 439.4 Hz */
-  //   // GPIO_WriteBit(GPIOC, GPIO_Pin_9, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_9)));
-  //   // capture = TIM_GetCapture4(TIM2);
-  //   // TIM_SetCompare4(TIM2, capture + CCR4_Val);
-  // }
-}
+//   //   // /* Pin PC.09 toggling with frequency = 439.4 Hz */
+//   //   // GPIO_WriteBit(GPIOC, GPIO_Pin_9, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_9)));
+//   //   // capture = TIM_GetCapture4(TIM2);
+//   //   // TIM_SetCompare4(TIM2, capture + CCR4_Val);
+//   // }
+// }
 
 /******************************************************************************/
 /*                 STM32F10x Peripherals Interrupt Handlers                   */
