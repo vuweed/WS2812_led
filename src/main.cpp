b978599b3da1992b0189b8ad0933c884a5f7762f
vuweed <halucminhvu2000@gmail.com>
2024-07-18 23:36:53 +0700

    not ok

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
-
+#include <math.h>
 /** @addtogroup STM32F10x_StdPeriph_Examples
   * @{
   */
@@ -40,52 +40,112 @@ TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 TIM_OCInitTypeDef  TIM_OCInitStructure;
 uint16_t SRC_Buffer[3] = {0, 0, 0};
 uint16_t TimerPeriod = 0;
+#define MAX_LED 100
+#define USE_BRIGHTNESS 0
 
+#define PI 3.14159265
+
+
+uint8_t LED_Data[MAX_LED][4];
+uint8_t LED_Mod[MAX_LED][4];  // for brightness
+
+
+void Set_Brightness (int brightness)  // 0-45
+{
+#if USE_BRIGHTNESS
+
+    if (brightness > 45) brightness = 45;
+    for (int i=0; i<MAX_LED; i++)
+    {
+        LED_Mod[i][0] = LED_Data[i][0];
+        for (int j=1; j<4; j++)
+        {
+            float angle = 90-brightness;  // in degrees
+            angle = angle*PI / 180;  // in rad
+            LED_Mod[i][j] = (LED_Data[i][j])/(tan(angle));
+        }
+    }
+
+#endif
+
+}
+
+// void led_move()
+// {
+//     int i = 0;
+//     int R = 0, G = 0, B = 0;
+//     for(i = 0; i < MAX_LED; i++)
+//     {
+//         Set_LED(i, R, G, B);
+//         Set_Brightness(10);
+//         WS2812_Send();
+//         HAL_Delay(30);
+//         if(R > 255)
+//             R = 0;
+//         if(G > 255)
+//             G = 0;
+//         if(B > 255)
+//             B = 0;
+
+//         R+=10;
+//         G+=10;
+//         B+=10;
+//     }
+// }
+uint16_t pwmData[(24*MAX_LED)+50];
 /* Private function prototypes -----------------------------------------------*/
 void RCC_Configuration(void);
 void GPIO_Configuration(void);
 void DMA_Configuration(void);
 
 /* Private functions ---------------------------------------------------------*/
-//void Wrap_buffer_led (void)
-//{
-//    uint32_t indx=0;
-//    uint32_t color;
-//
-//
-//    for (int i= 0; i<MAX_LED; i++)
-//    {
-//#if USE_BRIGHTNESS
-//        color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
-//#else
-//        color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
-//#endif
-//
-//        for (int i=23; i>=0; i--)
-//        {
-//            if (color&(1<<i))
-//            {
-//                pwmData[indx] = 60;  // 2/3 of 90
-//            }
-//
-//            else pwmData[indx] = 30;  // 1/3 of 90
-//
-//            indx++;
-//        }
-//
-//    }
-//
-//    for (int i=0; i<50; i++)
-//    {
-//        pwmData[indx] = 0;
-//        indx++;
-//    }
-//
-////    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx);
-////    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_2, (uint32_t *)pwmData, indx);
-////    while (!datasentflag){};
-////    datasentflag = 0;
-//}
+void Set_LED (int LEDnum, int Red, int Green, int Blue)
+{
+    LED_Data[LEDnum][0] = LEDnum;
+    LED_Data[LEDnum][1] = Green;
+    LED_Data[LEDnum][2] = Red;
+    LED_Data[LEDnum][3] = Blue;
+}
+
+void Wrap_buffer_led (void)
+{
+   uint32_t indx=0;
+   uint32_t color;
+
+
+   for (int i= 0; i<MAX_LED; i++)
+   {
+#if USE_BRIGHTNESS
+       color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
+#else
+       color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
+#endif
+
+       for (int i=23; i>=0; i--)
+       {
+           if (color&(1<<i))
+           {
+               pwmData[indx] = 60;  // 2/3 of 90
+           }
+
+           else pwmData[indx] = 30;  // 1/3 of 90
+
+           indx++;
+       }
+
+   }
+
+   for (int i=0; i<50; i++)
+   {
+       pwmData[indx] = 0;
+       indx++;
+   }
+
+//    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx);
+//    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_2, (uint32_t *)pwmData, indx);
+//    while (!datasentflag){};
+//    datasentflag = 0;
+}
 /**
   * @brief  Main program
   * @param  None
@@ -126,7 +186,7 @@ int main(void)
   value defined by the SRC_Buffer .
   -----------------------------------------------------------------------------*/
   /* Compute the value to be set in ARR register to generate signal frequency at 17.57 Khz */
-  TimerPeriod = (SystemCoreClock / 17570 ) - 1;
+  TimerPeriod = (SystemCoreClock / 800000 ) - 1;
   /* Compute CCR1 value to generate a duty cycle at 50% */
   SRC_Buffer[0] = (uint16_t) (((uint32_t) 5 * (TimerPeriod - 1)) / 10);
   /* Compute CCR1 value to generate a duty cycle at 37.5% */
@@ -144,6 +204,8 @@ int main(void)
 
   TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
 
+
+
   /* Channel 3 Configuration in PWM mode */
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
@@ -153,8 +215,18 @@ int main(void)
   TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_Low;
   TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
   TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
+  //config led
+  int i = 0;
+    for(i = 0; i < MAX_LED; i++)
+    {
+        Set_LED(i, 255, 0, 0);
+    }
 
+    Wrap_buffer_led();
+    TIM_OCInitStructure.TIM_Pulse = pwmData[0];
   TIM_OC3Init(TIM1, &TIM_OCInitStructure);
+
+
 
   /* TIM1 Update DMA Request enable */
   TIM_DMACmd(TIM1, TIM_DMA_Update, ENABLE);
@@ -216,9 +288,9 @@ void DMA_Configuration(void)
   DMA_DeInit(DMA1_Channel5);
 
   DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)TIM1_CCR3_Address;
-  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)SRC_Buffer;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)pwmData;
   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-  DMA_InitStructure.DMA_BufferSize = 3;
+  DMA_InitStructure.DMA_BufferSize = (24*MAX_LED)+50;
   DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
   DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
   DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
