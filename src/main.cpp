b952f762e75731bb53397eff1ad6c8ea53e360eb
vuweed <halucminhvu2000@gmail.com>
2024-07-21 09:51:03 +0700

    format code

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,16 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-GPIO_InitTypeDef         GPIO_InitStructure;
-DMA_InitTypeDef          DMA_InitStructure;
-TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-TIM_OCInitTypeDef        TIM_OCInitStructure;
+GPIO_InitTypeDef GPIO_InitStructure;
+DMA_InitTypeDef DMA_InitStructure;
+TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+TIM_OCInitTypeDef TIM_OCInitStructure;
 #define MAX_LED 120
 #define USE_BRIGHTNESS 0
-#define DMA_BUF_SIZE 	(3 * ((24 * MAX_LED) + 50))
+#define DMA_BUF_SIZE (3 * ((24 * MAX_LED) + 50))
 uint16_t pwmData[((24 * MAX_LED) + 50)];
-//uint16_t SRC_Buffer[DMA_BUF_SIZE] = {0};
-//#define MAX_LED 1
+// uint16_t SRC_Buffer[DMA_BUF_SIZE] = {0};
+// #define MAX_LED 1
 #define USE_BRIGHTNESS 0
 
 #define PI 3.14159265
@@ -86,128 +86,126 @@ void Wrap_buffer_led(void)
 	//    datasentflag = 0;
 }
 /**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
+ * @brief  Main program
+ * @param  None
+ * @retval None
+ */
 int main(void)
 {
-  /* TIM1 and GPIOA clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 | RCC_APB2Periph_GPIOA, ENABLE);
+	/* TIM1 and GPIOA clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 | RCC_APB2Periph_GPIOA, ENABLE);
 
-  /* DMA clock enable */
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	/* DMA clock enable */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 
-  /* GPIOA Configuration: Channel 1 as alternate function push-pull */
-  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_8;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+	/* GPIOA Configuration: Channel 1 as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-//  /* TIM1 DeInit */
-//  TIM_DeInit(TIM1);
-//
-//  /* DMA1 Channel5 Config */
-//  DMA_DeInit(DMA1_Channel5);
+	//  /* TIM1 DeInit */
+	//  TIM_DeInit(TIM1);
+	//
+	//  /* DMA1 Channel5 Config */
+	//  DMA_DeInit(DMA1_Channel5);
 
-  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)TIM1_DMAR_ADDRESS;
-  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)pwmData;
-  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-  DMA_InitStructure.DMA_BufferSize = ((24 * MAX_LED) + 50);
-  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
-  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-//   DMA_Init(DMA1_Channel5, &DMA_InitStructure);
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)TIM1_DMAR_ADDRESS;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)pwmData;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+	DMA_InitStructure.DMA_BufferSize = ((24 * MAX_LED) + 50);
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	//   DMA_Init(DMA1_Channel5, &DMA_InitStructure);
 
-  /* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = 90;
-  TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) (SystemCoreClock / 72000000) - 1;
-  TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;
-  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-//   TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+	/* Time base configuration */
+	TIM_TimeBaseStructure.TIM_Period = 90;
+	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t)(SystemCoreClock / 72000000) - 1;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	//   TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
 
-  /* TIM Configuration in PWM Mode */
-  TIM_OCInitStructure.TIM_OCMode =  TIM_OCMode_PWM1;
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-  TIM_OCInitStructure.TIM_Pulse = 90;
-//   TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+	/* TIM Configuration in PWM Mode */
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 90;
+	//   TIM_OC1Init(TIM1, &TIM_OCInitStructure);
 
-  /* TIM1 DMAR Base register and DMA Burst Length Config */
-//   TIM_DMAConfig(TIM1, TIM_DMABase_CCR1, TIM_DMABurstLength_1Transfer);
+	/* TIM1 DMAR Base register and DMA Burst Length Config */
+	//   TIM_DMAConfig(TIM1, TIM_DMABase_CCR1, TIM_DMABurstLength_1Transfer);
 
+	/* Infinite loop */
+	while (1)
+	{
+		/* TIM1 DeInit */
+		TIM_DeInit(TIM1);
 
-
-  /* Infinite loop */
-  while(1)
-  {
-      /* TIM1 DeInit */
-      TIM_DeInit(TIM1);
-
-      /* DMA1 Channel5 Config */
-      DMA_DeInit(DMA1_Channel5);
-	DMA_Init(DMA1_Channel5, &DMA_InitStructure);
-	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
-	TIM_OC1Init(TIM1, &TIM_OCInitStructure);
-	TIM_DMAConfig(TIM1, TIM_DMABase_CCR1, TIM_DMABurstLength_1Transfer);
-        if(R == 255)
-        {
-            R= 0;
-
-        }
-        else
-        {
-            R= 255;
-        }
+		/* DMA1 Channel5 Config */
+		DMA_DeInit(DMA1_Channel5);
+		DMA_Init(DMA1_Channel5, &DMA_InitStructure);
+		TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+		TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+		TIM_DMAConfig(TIM1, TIM_DMABase_CCR1, TIM_DMABurstLength_1Transfer);
+		if (R == 255)
+		{
+			R = 0;
+		}
+		else
+		{
+			R = 255;
+		}
 		for (int i = 0; i < MAX_LED; i++)
 		{
 
 			Set_LED(i, R, G, B);
 		}
 		Wrap_buffer_led();
-	/* TIM1 DMA Update enable */
-	TIM_DMACmd(TIM1, TIM_DMA_Update, ENABLE);
+		/* TIM1 DMA Update enable */
+		TIM_DMACmd(TIM1, TIM_DMA_Update, ENABLE);
 
-	/* TIM1 enable */
-	TIM_Cmd(TIM1, ENABLE);
+		/* TIM1 enable */
+		TIM_Cmd(TIM1, ENABLE);
 
-	/* TIM1 PWM Outputs Enable */
-	TIM_CtrlPWMOutputs(TIM1, ENABLE);
+		/* TIM1 PWM Outputs Enable */
+		TIM_CtrlPWMOutputs(TIM1, ENABLE);
 
-	/* DMA1 Channel5 enable */
-	DMA_Cmd(DMA1_Channel5, ENABLE);
+		/* DMA1 Channel5 enable */
+		DMA_Cmd(DMA1_Channel5, ENABLE);
 
-	/* Wait until DMA1 Channel5 end of Transfer */
-	while (!DMA_GetFlagStatus(DMA1_FLAG_TC5))
-	{
+		/* Wait until DMA1 Channel5 end of Transfer */
+		while (!DMA_GetFlagStatus(DMA1_FLAG_TC5))
+		{
+		}
+
+		// Disable the timer and DMA after the condition is met
+		TIM_CtrlPWMOutputs(TIM1, DISABLE);
+		DMA_Cmd(DMA1_Channel5, DISABLE);
+		TIM_Cmd(TIM1, DISABLE);
+		TIM_DMACmd(TIM1, TIM_DMA_Update, DISABLE);
 	}
-
-	// Disable the timer and DMA after the condition is met
-	TIM_CtrlPWMOutputs(TIM1, DISABLE);
-	DMA_Cmd(DMA1_Channel5, DISABLE);
-	TIM_Cmd(TIM1, DISABLE);
-	TIM_DMACmd(TIM1, TIM_DMA_Update, DISABLE);
-  }
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* User can add his own implementation to report the file name and line number,
+	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  while (1)
-  {}
+	while (1)
+	{
+	}
 }
 #endif
